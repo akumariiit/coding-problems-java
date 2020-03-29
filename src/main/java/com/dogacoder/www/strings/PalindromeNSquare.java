d2c398ceaf400e39a5682693e4f4a52fff77c859
Amit Kumar <akumariiit@gmail.com>
2020-03-29 14:07:58 +0530

    optimized palindrome length problem

---
+++ src/main/java/com/dogacoder/www/strings/PalindromeNSquare.java
@@ -0,0 +1,46 @@
+package com.dogacoder.www.strings;
+
+public class PalindromeNSquare {
+
+    void printSubStr(String str, int low, int high) {
+        System.out.println(str.substring(low, high + 1));
+    }
+
+    int longestPalSubstr(String str) {
+        int maxLength = 1; // The result (length of LPS)
+        int start = 0;
+        int len = str.length();
+        int low, high;
+
+        for (int i = 1; i < len; ++i)
+        {
+            low = i - 1;
+            high = i;
+            while (low >= 0 && high < len && str.charAt(low) == str.charAt(high)) {
+                if (high - low + 1 > maxLength) {
+                    start = low;
+                    maxLength = high - low + 1;
+                }
+                --low;
+                ++high;
+            }
+
+            low = i - 1;
+            high = i + 1;
+            while (low >= 0 && high < len && str.charAt(low) == str.charAt(high)) {
+                if (high - low + 1 > maxLength) {
+                    start = low;
+                    maxLength = high - low + 1;
+                }
+                --low;
+                ++high;
+            }
+        }
+
+        System.out.print("Longest palindrome substring is: ");
+        printSubStr(str, start, start + maxLength - 1);
+
+        return maxLength;
+    }
+
+}
