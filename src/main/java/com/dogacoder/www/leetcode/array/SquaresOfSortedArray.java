46be2389557d3c2268faa34c7f9a5a58db9399d8
Amit Kumar <akumariiit@gmail.com>
2020-05-02 16:35:06 +0530

    Squares of a Sorted Array

---
+++ src/main/java/com/dogacoder/www/leetcode/array/SquaresOfSortedArray.java
@@ -0,0 +1,26 @@
+package com.dogacoder.www.leetcode.array;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class SquaresOfSortedArray {
+    public int[] sortedSquares(int[] A) {
+        for (int i=0; i<A.length; i++) {
+            if (A[i] < 0) {
+                A[i] = -A[i];
+            }
+        }
+        Arrays.sort(A);
+        for (int i=0; i<A.length; i++) {
+            A[i] = A[i] * A[i];
+        }
+        return A;
+    }
+
+    @Test
+    public void test() {
+        int[] a = {1, 2, 3};
+        System.out.println(sortedSquares(a));
+    }
+}
