cec4d3a70728297c81abcdf6d8fbd08a725f7d7a
Amit Kumar <akumariiit@gmail.com>
2020-05-02 13:20:16 +0530

    First Bas Version

---
+++ src/main/java/com/dogacoder/www/leetcode/may2020/JewelsandStones_May2.java
@@ -0,0 +1,4 @@
+package com.dogacoder.www.leetcode.may2020;
+
+public class JewelsandStones_May2 {
+}
---
+++ src/main/java/com/dogacoder/www/leetcode/may2020/VersionControl.java
@@ -0,0 +1,7 @@
+package com.dogacoder.www.leetcode.may2020;
+
+public class VersionControl {
+    boolean isBadVersion(int version) {
+        return true;
+    }
+}
