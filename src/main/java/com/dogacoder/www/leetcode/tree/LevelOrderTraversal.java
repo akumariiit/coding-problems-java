b631065264293de4b927a9d1e452ce333fdc2fd3
Amit Kumar <akumariiit@gmail.com>
2020-05-05 15:36:49 +0530

    level order traversal of bst

---
+++ src/main/java/com/dogacoder/www/leetcode/tree/LevelOrderTraversal.java
@@ -0,0 +1,74 @@
+package com.dogacoder.www.leetcode.tree;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ *
+ * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+ *
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *
+ * return its level order traversal as:
+ *
+ * [
+ *   [3],
+ *   [9,20],
+ *   [15,7]
+ * ]
+ *
+ */
+public class LevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(null);
+        List<Integer> temp = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            if (queue.peek() == null) {
+                queue.poll();
+                if (queue.size() > 0) {
+                    queue.add(null);
+                }
+                list.add(temp);
+                temp = new ArrayList<>();
+            }
+            else {
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        return list;
+    }
+
+    @Test
+    public void test() {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(levelOrder(root));
+        assertTrue(levelOrder(root).size() == 3);
+    }
+}
