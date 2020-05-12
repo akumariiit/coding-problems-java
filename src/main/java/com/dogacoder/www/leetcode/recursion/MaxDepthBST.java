1e39bb2466068dbd3e8c041244e81bc99bbe0d38
Amit Kumar <akumariiit@gmail.com>
2020-05-12 21:57:56 +0530

    Max depth in BST

--- src/main/java/com/dogacoder/www/leetcode/recursion/MaxDepthBST.java
+++ src/main/java/com/dogacoder/www/leetcode/recursion/MaxDepthBST.java
@@ -1,4 +1,11 @@
 package com.dogacoder.www.leetcode.recursion;
 
 public class MaxDepthBST {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+
+    }
 }
