e659bfdccb94756f18f509437b3e58ae8d2a723a
Amit Kumar <akumariiit@gmail.com>
2020-06-28 17:20:51 +0530

    [Dynamic] Count of Subset Sum --> More space optimized solution

--- src/main/java/com/dogacoder/www/educative/dynamic/CountOfSubsetSum.java
+++ src/main/java/com/dogacoder/www/educative/dynamic/CountOfSubsetSum.java
@@ -49,20 +49,6 @@ public class CountOfSubsetSum {
     // using memoization in top down approach
     // Time and Space complexity O(N*SUM) Since we are calculating for each subarray and for each sum
 
-    /**
-     *
-    We will try to find if we can make all possible sums with every subset to populate the array db[TotalNumbers][S+1].
-    So, at every step we have two options:
-
-    1. Exclude the number. Count all the subsets without the given number up to the given sum => dp[index-1][sum]
-    2. Include the number if its value is not more than the ‘sum’. In this case,
-     we will count all the subsets to get the remaining sum => dp[index-1][sum-num[index]]
-
-    To find the total sets, we will add both of the above two values:
-    `dp[index][sum] = dp[index-1][sum] + dp[index-1][sum-num[index]])`
-
-     **/
-
     public int countTopDownMemo(int[] nums, int sum) {
         if (nums == null || nums.length == 0) {
             return 0;
@@ -103,6 +89,20 @@ public class CountOfSubsetSum {
 
     // Time complexity O(N*Sum)
     // Space complexity O(N*Sum)
+    /**
+     *
+     We will try to find if we can make all possible sums with every subset to populate the array db[TotalNumbers][S+1].
+     So, at every step we have two options:
+
+     1. Exclude the number. Count all the subsets without the given number up to the given sum => dp[index-1][sum]
+     2. Include the number if its value is not more than the ‘sum’. In this case,
+     we will count all the subsets to get the remaining sum => dp[index-1][sum-num[index]]
+
+     To find the total sets, we will add both of the above two values:
+     `dp[index][sum] = dp[index-1][sum] + dp[index-1][sum-num[index]])`
+
+     **/
+
     public int countBottomUp(int[] nums, int sum) {
         if (nums == null || nums.length == 0) {
             return 0;
@@ -139,5 +139,57 @@ public class CountOfSubsetSum {
     public void test6() {
         int[] p = {1, 2, 7, 1, 5};
         assertEquals(countBottomUp(p, 9), 3);
+    }
+
+    // Time complexity O(N*Sum)
+    // Space complexity O(Sum)
+    /**
+     *
+     We will try to find if we can make all possible sums with every subset to populate the array db[TotalNumbers][S+1].
+     So, at every step we have two options:
+
+     1. Exclude the number. Count all the subsets without the given number up to the given sum => dp[index-1][sum]
+     2. Include the number if its value is not more than the ‘sum’. In this case,
+     we will count all the subsets to get the remaining sum => dp[index-1][sum-num[index]]
+
+     To find the total sets, we will add both of the above two values:
+     `dp[index][sum] = dp[index-1][sum] + dp[index-1][sum-num[index]])`
+
+     **/
+
+    public int countBottomUpSpaceOptimized(int[] nums, int sum) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        int[] dp = new int[sum+1];
+        dp[0] = 1;
+        for (int i = 1; i <= sum; i++) {
+            dp[i] = (i == nums[0]) ? 1 : 0;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int s = sum; s >= 0; s--) {
+                // excluding
+                dp[s] = dp[s];
+                // including
+                if (s >= nums[i]) {
+                    dp[s] += dp[s-nums[i]];
+                }
+            }
+        }
+        return dp[sum];
+    }
+
+    @Test
+    public void test7() {
+        int[] p = {1, 1, 2, 3};
+        assertEquals(countBottomUpSpaceOptimized(p, 4), 3);
+    }
+
+    @Test
+    public void test8() {
+        int[] p = {1, 2, 7, 1, 5};
+        assertEquals(countBottomUpSpaceOptimized(p, 9), 3);
     }
 }
