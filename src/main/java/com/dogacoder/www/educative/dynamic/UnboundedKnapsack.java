56deb2978a6b5ca16a7ba259462850aa25ec4265
Amit Kumar <amitkumar@expedia.com>
2020-07-07 00:09:36 +0530

    Unbounded Knapsack with Top Down approach using memoization

--- src/main/java/com/dogacoder/www/educative/dynamic/UnboundedKnapsack.java
+++ src/main/java/com/dogacoder/www/educative/dynamic/UnboundedKnapsack.java
@@ -58,4 +58,40 @@ public class UnboundedKnapsack {
         int c = 8;
         assertEquals(solveKnapsack(p, w, c), 140);
     }
+
+    public int solveKnapsackTopDown(int[] p, int[] w, int c) {
+        Integer[][] dp = new Integer[p.length][c+1];
+        return calcRecurTopDown(p, w, c, 0, dp);
+    }
+
+    private int calcRecurTopDown(int[] profit, int[] weight, int capacity, int index, Integer[][] dp) {
+        if (index >= profit.length || profit.length == 0 || weight.length != profit.length || capacity <= 0) {
+            return 0;
+        }
+        if (null == dp[index][capacity]) {
+            int exc = calcRecurTopDown(profit, weight, capacity, index+1, dp);
+            int inc = 0;
+            if (weight[index] <= capacity) {
+                inc = profit[index] + calcRecurTopDown(profit, weight, capacity - weight[index], index, dp);
+            }
+            dp[index][capacity] = Math.max(inc, exc);
+        }
+        return dp[index][capacity];
+    }
+
+    @Test
+    public void test3() {
+        int[] p = {15,20,50};
+        int[] w = {1,2,3};
+        int c = 5;
+        assertEquals(solveKnapsackTopDown(p, w, c), 80);
+    }
+
+    @Test
+    public void test4() {
+        int[] p = { 15, 50, 60, 90 };
+        int[] w = { 1, 3, 4, 5} ;
+        int c = 8;
+        assertEquals(solveKnapsackTopDown(p, w, c), 140);
+    }
 }
